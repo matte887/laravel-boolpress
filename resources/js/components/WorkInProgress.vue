f6cd44a613ffa8df77abe73174f2182cc36e5ac6
matte887 <m.erroi92@gmail.com>
2022-07-05 17:26:33 +0200

    Scaffolding front office

---
+++ resources/js/components/WorkInProgress.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Work in progress</div>
+</template>
+
+<script>
+export default {
+    name: 'WorkInProgress'
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>Ciao, sono il front office</h1>
+    <WorkInProgress />
+  </div>
+</template>
+
+<script>
+import WorkInProgress from "../components/WorkInProgress.vue";
+
+export default {
+  name: "app",
+  components: {
+    WorkInProgress
+  }
+};
+</script>
+
+<style>
+</style>
