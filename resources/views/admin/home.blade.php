7f97be20bdae0b72b25c49d3ee2211c908b18b0f
matte887 <m.erroi92@gmail.com>
2022-07-05 16:06:23 +0200

    Prelevato nome utente e passato alla view

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="container">
-        <h1>Ciao, sono la pagina controller dell'admin</h1>
+        <h1>Ciao {{ $user->name }}, sono la pagina controller dell'admin</h1>
     </div>
 @endsection
