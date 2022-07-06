7ca09f7c2f0baa07f7177d08af6c779960dcf918
matte887 <m.erroi92@gmail.com>
2022-07-06 16:05:29 +0200

    Creato index per i post lato admin

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="container">
+        <h2>Posts's list</h2>
+        <div class="row row-cols-3">
+            @foreach ($posts as $post)
+                <div class="col">
+                    <div class="card mb-5" style="width: 18rem;">
+                        {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            <a href="#" class="btn btn-primary">Show post</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
