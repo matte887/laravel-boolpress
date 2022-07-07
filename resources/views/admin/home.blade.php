8bd6c973841f703fabb3f100d279e23c4448e0f8
matte887 <m.erroi92@gmail.com>
2022-07-07 17:41:16 +0200

    Creato collegamento inverso userinfo->user

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="container">
         <h1>Ciao {{ $user->name }}, sono la pagina controller dell'admin</h1>
-        {{-- <p>{{ $userInfo->address }}</p> --}}
-        
+        {{-- @if ($userInfo)
+            <p>{{ $userInfo->address }}</p>
+        @endif --}}
+
     </div>
 @endsection
